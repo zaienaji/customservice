a0747c7ae46057ac2a1c0d1b83ccf3d767a37765
zaienaji <zaien.aji@gmail.com>
2022-07-28 06:23:31 +0800

    make jdbcTemplate autowired and automatically configured

--- inventory/src/main/java/com/infinite/inventory/InventoryServiceApplication.java
+++ inventory/src/main/java/com/infinite/inventory/InventoryServiceApplication.java
@@ -1,34 +1,10 @@
 package com.infinite.inventory;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-@PropertySource(value = "application.properties")
 public class InventoryServiceApplication {
-	
-	@Autowired
-	Environment env;
-	
-	@Bean("jdbcTemplate")
-	JdbcTemplate jdbcTemplate() {
-		DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-		dataSourceBuilder.driverClassName(env.getProperty("spring.datasource.driver-class-name"));
-		dataSourceBuilder.url(env.getProperty("spring.datasource.url"));
-		dataSourceBuilder.username(env.getProperty("spring.datasource.username"));
-		dataSourceBuilder.password(env.getProperty("spring.datasource.password"));
-		DataSource datasource = dataSourceBuilder.build();
-		
-		return new JdbcTemplate(datasource);
-	}
 
 	public static void main(String[] args) {
 		
--- inventory/src/main/java/com/infinite/inventory/MaterialTransactionController.java
+++ inventory/src/main/java/com/infinite/inventory/MaterialTransactionController.java
@@ -3,7 +3,6 @@ package com.infinite.inventory;
 import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
