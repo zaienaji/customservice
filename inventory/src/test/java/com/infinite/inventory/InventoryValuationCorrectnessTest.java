30a85ca3b4a7babf391e3af9d0ba80bc7d4f0f42
zaienaji <zaien.aji@gmail.com>
2022-07-30 15:40:21 +0800

    inventory valuation correctness test now using beforeAll to avoid multiple submission of MaterialTransaction

--- inventory/src/test/java/com/infinite/inventory/InventoryValuationCorrectnessTest.java
+++ inventory/src/test/java/com/infinite/inventory/InventoryValuationCorrectnessTest.java
@@ -9,8 +9,8 @@ import java.nio.file.Files;
 import java.util.HashMap;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,56 +25,53 @@ import org.springframework.test.context.jdbc.Sql;
 
 import com.infinite.inventory.sharedkernel.CostingStatus;
 
-
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class InventoryValuationCorrectnessTest {
-	
+
 	@LocalServerPort
 	private int port;
 
 	@Autowired
 	TestRestTemplate restTemplate;
-	
+
 	@ParameterizedTest
-	@CsvSource({
-		"D8B2916974AE4F3D80C79FAC27E2EB2D,Calculated,10000",
+	@CsvSource({ 
+		"D8B2916974AE4F3D80C79FAC27E2EB2D,Calculated,10000", 
 		"1FE6DDD27E014FAC8DD53F5C01892851,Calculated,8000",
 		"174C8CE7718643A9AA487E16CD29B55A,Error,",
-		"D8DDDF7EBF274F749E7C5CE5FE393D8F,,"})
-	@Sql({"/schema.sql"})
-	public void testInventoryValuationCorrectness(String correlationId, CostingStatus costingStatus, Double acquisitionCost) throws Exception {
-		
-		testPostMaterialTransaction();
-		testCorrectnessAcquisitionCostCalculation(correlationId, costingStatus, acquisitionCost==null ? null : new BigDecimal(acquisitionCost));
-	}
+		"D8DDDF7EBF274F749E7C5CE5FE393D8F,," })
+	@Sql({ "/schema.sql" })
+	public void testInventoryValuationCorrectness(String correlationId, CostingStatus costingStatus,
+	    Double acquisitionCost) throws Exception {
 
-	private void testCorrectnessAcquisitionCostCalculation(String correlationId, CostingStatus costingStatus, BigDecimal acquisitionCost) throws IOException, JSONException {
-		String url = "http://localhost:" + port + "/api/inventory/materialtransaction?materialTransactionCorellationIds="+correlationId;
-		
+		String url = "http://localhost:" + this.port + "/api/inventory/materialtransaction?materialTransactionCorellationIds="
+		    + correlationId;
+
 		HashMap<String, String> param = new HashMap<>();
 		param.put("materialTransactionCorellationIds", correlationId);
 
 		String body = this.restTemplate.getForObject(url, String.class);
 		JSONArray responseBody = new JSONArray(body);
-		if (costingStatus==null) {
-		
-			assertThat(responseBody.length()==0);
+		if (costingStatus == null) {
+
+			assertThat(responseBody.length() == 0);
 			return;
 		}
-		
+
 		assertThat(responseBody.length() >= 1);
-		
+
 		JSONObject mTransaction = responseBody.getJSONObject(0);
-		CostingStatus actualCostingStatus = CostingStatus.valueOf((String)mTransaction.get("costingStatus"));
+		CostingStatus actualCostingStatus = CostingStatus.valueOf((String) mTransaction.get("costingStatus"));
 		assertThat(actualCostingStatus == costingStatus);
-		
-		if (costingStatus==CostingStatus.Calculated) {
-			BigDecimal actualAcquisitionCost = new BigDecimal((Double)mTransaction.get("acquisitionCost"));
-			assertThat(acquisitionCost).isEqualTo(actualAcquisitionCost);
+
+		if (costingStatus == CostingStatus.Calculated) {
+			BigDecimal actualAcquisitionCost = new BigDecimal((Double) mTransaction.get("acquisitionCost"));
+			assertThat(actualAcquisitionCost).isEqualTo(new BigDecimal(acquisitionCost));
 		}
 	}
 
-	private void testPostMaterialTransaction() throws IOException {
+	@BeforeAll
+	public static void testPostMaterialTransaction(@Autowired TestRestTemplate restTemplate, @LocalServerPort int port) throws IOException {
 		String testdata = getMaterialCostingTestData();
 
 		HttpHeaders headers = new HttpHeaders();
@@ -84,13 +81,13 @@ class InventoryValuationCorrectnessTest {
 
 		String url = "http://localhost:" + port + "/api/inventory/valuation";
 
-		String result = this.restTemplate.postForObject(url, request, String.class);
+		String result = restTemplate.postForObject(url, request, String.class);
 
 		assertThat(result).isEqualTo("inventory transaction received, lenght: 26");
 
 	}
 
-	private String getMaterialCostingTestData() throws IOException {
+	private static String getMaterialCostingTestData() throws IOException {
 		File resource = new ClassPathResource("testdata.json").getFile();
 		String testdata = new String(Files.readAllBytes(resource.toPath()));
 		return testdata;
