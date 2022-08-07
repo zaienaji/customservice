b2810e03646983d17d1a924a7309f9a66591de45
zaienaji <zaien.aji@gmail.com>
2022-08-07 10:04:09 +0800

    issue #13: limit thread pool size to half of available cores

--- inventory/src/main/java/com/infinite/inventory/ThreadPool.java
+++ inventory/src/main/java/com/infinite/inventory/ThreadPool.java
@@ -8,7 +8,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class ThreadPool {
 	
-	private final ExecutorService pool = Executors.newFixedThreadPool(1_000_000);
+	private final ExecutorService pool;
+	
+	public ThreadPool() {
+		
+		int cores = Runtime.getRuntime().availableProcessors();
+		if (cores>=4)
+			cores = cores/2;
+			
+		pool = Executors.newFixedThreadPool(cores);
+	}
 	
 	public void execute(Runnable command) {
 		pool.execute(command);
